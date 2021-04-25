ac3ec160bedf8a850918c5eba88799ae9d556801
Andrew Stewart <andrew.c.stewart@gmail.com>
2021-04-25 17:47:13 +0000

    debug

--- target_athena/__init__.py
+++ target_athena/__init__.py
@@ -133,9 +133,9 @@ def persist_messages(messages, config, s3_client, athena_client):
                             .format(o['type'], o))
 
     # Create schemas in Athena
-    logger.debug("schemas: {}".format(schemas))
+    logger.info("schemas: {}".format(schemas))
     for stream, schema in schemas.items():
-        logger.debug("schema: {}".format(schema))
+        logger.info("schema: {}".format(schema))
         # data_location = "s3://{s3_bucket}/{target_key}".format(s3_bucket=config.get('s3_bucket'), target_key=target_key) # TODO: double check this
         # ddl = utils.generate_json_table_statement(stream, schema, 
         #                                     database = 'dw', 
