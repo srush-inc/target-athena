c5b189363979aeb7d9f5d62b0220eae1e200c621
Andrew Stewart <andrew.c.stewart@gmail.com>
2021-04-25 04:20:04 +0000

    added json schema -> ddl helpers

--- target_athena/utils.py
+++ target_athena/utils.py
@@ -122,7 +122,7 @@ def flatten_record(d, parent_key=[], sep='__'):
 def get_target_key(message, prefix=None, timestamp=None, naming_convention=None):
     """Creates and returns an S3 key for the message"""
     if not naming_convention:
-        naming_convention = '{stream}-{timestamp}.csv' # o['stream'] + '-' + now + '.csv'
+        naming_convention = '{stream}/{timestamp}.csv' # o['stream'] + '-' + now + '.csv'
     if not timestamp:
         timestamp = datetime.now().strftime('%Y%m%dT%H%M%S')
     key = naming_convention
@@ -143,3 +143,70 @@ def get_target_key(message, prefix=None, timestamp=None, naming_convention=None)
         filename = key.split('/')[-1]
         key = key.replace(filename, f'{prefix}{filename}')
     return key
+
+# This function is borrowed direclty from https://github.com/datadudes/json2hive/blob/master/json2hive/generators.py
+def generate_field_definitions(schema, level=0):
+    keywords = ['timestamp', 'date', 'datetime']
+    tab = "  "
+    type_separator = " " if level == 0 else ": "
+    field_separator = "\n" if level == 0 else ",\n"
+    field_definitions = []
+    new_level = level + 1
+    indentation = new_level * tab
+    for name, attributes in schema.items():
+        cleaned_name = "`{}`".format(name) if name.lower() in keywords else name
+        if attributes['type'] == 'object':
+            field_definitions.append("{indentation}{name}{separator}STRUCT<\n{definitions}\n{indentation}>".format(
+                indentation=indentation,
+                name=cleaned_name,
+                separator=type_separator,
+                definitions=generate_field_definitions(attributes['properties'], new_level)
+            ))
+        elif attributes['type'] == 'array':
+            extra_indentation = (new_level + 1) * tab
+            if attributes['items']['type'] == 'object':
+                closing_bracket = "\n" + indentation + ">"
+                array_type = "STRUCT<\n{definitions}\n{indentation}>".format(
+                    indentation=extra_indentation,
+                    definitions=generate_field_definitions(attributes['items']['properties'], new_level + 1)
+                )
+            else:
+                closing_bracket = ">"
+                array_type = attributes['items']['type'].upper()
+            field_definitions.append("{indentation}{name}{separator}ARRAY<{definitions}{closing_bracket}".format(
+                indentation=indentation,
+                name=cleaned_name,
+                separator=type_separator,
+                definitions=array_type,
+                closing_bracket=closing_bracket
+            ))
+        else:
+            field_definitions.append("{indentation}{name}{separator}{type}".format(
+                indentation=indentation,
+                name=cleaned_name,
+                separator=type_separator,
+                type=attributes['type'].upper()
+            ))
+    return field_separator.join(field_definitions)
+
+# This function is borrowed direclty from https://github.com/datadudes/json2hive/blob/master/json2hive/generators.py
+def generate_json_table_statement(table, schema, data_location='', database='default', 
+                                  external=True, serde='org.apache.hadoop.hive.serde2.OpenCSVSerde'):
+    field_definitions = generate_field_definitions(schema['properties'])
+    external_marker = "EXTERNAL " if not external else ""
+    row_format = "ROW FORMAT SERDE '{serde}'".format(serde=serde) if serde else ""
+    stored = "\nSTORED AS TEXTFILE"
+    location = "\nLOCATION '{}'".format(data_location) if external else ''
+    statement = """CREATE {external_marker}TABLE IF NOT EXISTS {database}.{table} (
+{field_definitions}
+)
+{row_format}{stored}{location};""".format(
+        external_marker=external_marker,
+        database=database,
+        table=table,
+        field_definitions=field_definitions,
+        row_format=row_format,
+        stored=stored,
+        location=location
+    )
+    return statement
