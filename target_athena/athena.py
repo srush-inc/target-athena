f0efcad0d62e3fc88752bedf507cb0bcbd50a662
Andrew Stewart <andrew.c.stewart@gmail.com>
2021-04-25 18:53:15 +0000

    debug

--- target_athena/athena.py
+++ target_athena/athena.py
@@ -12,12 +12,14 @@ def create_client(config):
     aws_secret_access_key = config.get('aws_secret_access_key') or os.environ.get('AWS_SECRET_ACCESS_KEY')
     aws_session_token = config.get('aws_session_token') or os.environ.get('AWS_SESSION_TOKEN')
     aws_profile = config.get('aws_profile') or os.environ.get('AWS_PROFILE')
+    aws_region = config.get('aws_region') or os.environ.get('AWS_REGION')
     s3_staging_dir = config.get('s3_staging_dir') or os.environ.get('S3_STAGING_DIR')
 
     # AWS credentials based authentication
     if aws_access_key_id and aws_secret_access_key:
         cursor = connect(aws_access_key_id=aws_access_key_id,
                         aws_secret_access_key=aws_secret_access_key,
+                        region_name=aws_region,
                         s3_staging_dir=s3_staging_dir).cursor()
 
     # AWS Profile based authentication
